2bf08c482a008eca0bd6454e0cb79fec2b2d7ae1
AARINCONES <aarincones@uninorte.edu.co>
2022-12-12 20:58:04 -0500

    ..

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,1 +1,10 @@
-<p>hola esto es el dashboard</p>
+<?php require_once "../Template/cabecera.php" ?>
+
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Contenido principal</h1>
+   
+
+</div>
+<!--FIN del cont principal-->
+<?php require_once "../Template/footer.php" ?>
